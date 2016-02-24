ab01858e34fb3f8aa059e2ef53806406d10fdfc0
fusillicode <fusillicode@gmail.com>
2016-02-24 14:41:44 +0100

    Create users table

---
+++ db/migrate/20160224134049_create_users.rb
@@ -0,0 +1,11 @@
+class CreateUsers < ActiveRecord::Migration
+  def change
+    create_table :users do |t|
+      t.string :username, null: false
+      t.string :password_digest, null: false
+      t.integer :role, default: 0
+      t.timestamps null: false
+    end
+    add_index :users, :username, unique: true
+  end
+end
