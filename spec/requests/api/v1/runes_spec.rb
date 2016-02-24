9a359f8324e503e0f9fd9498de08e6b7b084b702
fusillicode <fusillicode@gmail.com>
2016-02-24 19:30:48 +0100

    Add specs enforcing authentication for runes resource

--- spec/requests/api/v1/runes_spec.rb
+++ spec/requests/api/v1/runes_spec.rb
@@ -4,43 +4,59 @@ describe 'Api::V1::Runes' do
   let(:admin) { create(:user, :admin) }
 
   describe 'GET /api/runes' do
-    let!(:runes) { create_list :rune, 2 }
-    before { get api_runes_path, nil, authorization: token_header(admin) }
+    context 'when not authenticated' do
+      it_behaves_like 'render_unauthorized' do
+        def action() get api_runes_path end
+      end
+    end
 
-    it { expect(response).to be_ok }
-    it { expect(response).to match_response_schema 'runes' }
+    context 'when authenticated' do
+      let!(:runes) { create_list :rune, 2 }
+      before { get api_runes_path, nil, authorization: token_header(admin) }
 
-    it 'shows all the runes' do
-      expect(json_response.map { |u| u[:name] }).to eq runes.map(&:name)
+      it { expect(response).to be_ok }
+      it { expect(response).to match_response_schema 'runes' }
+
+      it 'shows all the runes' do
+        expect(json_response.map { |u| u[:name] }).to eq runes.map(&:name)
+      end
     end
   end
 
   describe 'GET /api/runes/:id' do
     let!(:existing_rune) { create :rune }
 
-    context 'when the rune does not exist' do
-      before do
-        get api_rune_path(id: Rune.maximum(:id).next),
-            nil,
-            authorization: token_header(admin)
+    context 'when not authenticated' do
+      it_behaves_like 'render_unauthorized' do
+        def action() get api_rune_path(existing_rune) end
       end
-
-      it { expect(response).to be_not_found }
-      it { expect(response).to match_response_schema 'not_found' }
     end
 
-    context 'when the rune exists' do
-      before do
-        get api_rune_path(existing_rune),
-            nil,
-            authorization: token_header(admin)
+    context 'when authenticated' do
+      context 'when the rune does not exist' do
+        before do
+          get api_rune_path(id: Rune.maximum(:id).next),
+              nil,
+              authorization: token_header(admin)
+        end
+
+        it { expect(response).to be_not_found }
+        it { expect(response).to match_response_schema 'not_found' }
       end
 
-      it { expect(response).to be_ok }
-      it { expect(response).to match_response_schema 'rune' }
+      context 'when the rune exists' do
+        before do
+          get api_rune_path(existing_rune),
+              nil,
+              authorization: token_header(admin)
+        end
 
-      it 'shows the rune' do
-        expect(json_response[:name]).to eq existing_rune.name
+        it { expect(response).to be_ok }
+        it { expect(response).to match_response_schema 'rune' }
+
+        it 'shows the rune' do
+          expect(json_response[:name]).to eq existing_rune.name
+        end
       end
     end
   end
@@ -48,54 +64,65 @@ describe 'Api::V1::Runes' do
   describe 'PATCH/PUT /api/runes/:id' do
     let!(:existing_rune) { create :rune }
 
-    context 'when the rune does not exist' do
-      before do
-        patch api_rune_path(id: Rune.maximum(:id).next),
-              nil,
-              authorization: token_header(admin)
-      end
-
-      it { expect(response).to be_not_found }
-      it { expect(response).to match_response_schema 'not_found' }
-    end
-
-    context 'when the rune exists' do
-      context 'when supplying valid data' do
-        before do
+    context 'when not authenticated' do
+      it_behaves_like 'render_unauthorized' do
+        def action()
           patch api_rune_path(existing_rune),
-                { rune: { name: 'new-name' } },
-                authorization: token_header(admin)
+                { rune: { name: 'new-name' } }
         end
-
-        it { expect(response).to be_ok }
-        it { expect(response).to match_response_schema 'rune' }
-        it { expect(json_response[:name]).to eq existing_rune.reload.name }
-      end
-
-      context 'when supplying an invalid runename' do
-        before do
-          patch api_rune_path(existing_rune),
-                { rune: { name: '' } },
-                authorization: token_header(admin)
-        end
-
-        it { expect(response).to be_unprocessable }
       end
     end
 
-    context 'when non admin' do
-      let!(:user) { create :user }
-
-      context 'when updating another user rune' do
-        let!(:another_user_rune) { create :rune, user: create(:user) }
+    context 'when authenticated' do
+      context 'when the rune does not exist' do
         before do
-          delete api_rune_path(another_user_rune),
-                 nil,
-                 authorization: token_header(user)
+          patch api_rune_path(id: Rune.maximum(:id).next),
+                nil,
+                authorization: token_header(admin)
         end
 
-        it { expect(response).to be_forbidden }
-        it { expect(response).to match_response_schema 'forbidden' }
+        it { expect(response).to be_not_found }
+        it { expect(response).to match_response_schema 'not_found' }
+      end
+
+      context 'when the rune exists' do
+        context 'when supplying valid data' do
+          before do
+            patch api_rune_path(existing_rune),
+                  { rune: { name: 'new-name' } },
+                  authorization: token_header(admin)
+          end
+
+          it { expect(response).to be_ok }
+          it { expect(response).to match_response_schema 'rune' }
+          it { expect(json_response[:name]).to eq existing_rune.reload.name }
+        end
+
+        context 'when supplying an invalid runename' do
+          before do
+            patch api_rune_path(existing_rune),
+                  { rune: { name: '' } },
+                  authorization: token_header(admin)
+          end
+
+          it { expect(response).to be_unprocessable }
+        end
+      end
+
+      context 'when non admin' do
+        let!(:user) { create :user }
+
+        context 'when updating another user rune' do
+          let!(:another_user_rune) { create :rune, user: create(:user) }
+          before do
+            delete api_rune_path(another_user_rune),
+                   nil,
+                   authorization: token_header(user)
+          end
+
+          it { expect(response).to be_forbidden }
+          it { expect(response).to match_response_schema 'forbidden' }
+        end
       end
     end
   end
@@ -103,40 +130,50 @@ describe 'Api::V1::Runes' do
   describe 'DELETE /api/runes/:id' do
     let!(:existing_rune) { create :rune }
 
-    context 'when the rune does not exist' do
-      before do
-        delete api_rune_path(id: Rune.maximum(:id).next),
-               nil,
-               authorization: token_header(admin)
+    context 'when not authenticated' do
+      it_behaves_like 'render_unauthorized' do
+        def action()
+          delete api_rune_path(existing_rune)
+        end
       end
-
-      it { expect(response).to be_not_found }
-      it { expect(response).to match_response_schema 'not_found' }
     end
 
-    context 'when the rune exists' do
-      before do
-        delete api_rune_path(existing_rune),
-               nil,
-               authorization: token_header(admin)
-      end
-
-      it { expect(response.status).to eq 204 }
-      it { expect(response.message).to eq 'No Content' }
-      it { expect(Rune.find_by id: existing_rune.id).to be_nil }
-    end
-
-    context 'when non admin' do
-      let!(:user) { create :user }
-
-      context 'when deleting another user rune' do
-        let!(:another_user_rune) { create :rune, user: create(:user) }
+    context 'when authenticated' do
+      context 'when the rune does not exist' do
         before do
-          delete api_rune_path(another_user_rune), nil, authorization: token_header(user)
+          delete api_rune_path(id: Rune.maximum(:id).next),
+                 nil,
+                 authorization: token_header(admin)
         end
 
-        it { expect(response).to be_forbidden }
-        it { expect(response).to match_response_schema 'forbidden' }
+        it { expect(response).to be_not_found }
+        it { expect(response).to match_response_schema 'not_found' }
+      end
+
+      context 'when the rune exists' do
+        before do
+          delete api_rune_path(existing_rune),
+                 nil,
+                 authorization: token_header(admin)
+        end
+
+        it { expect(response.status).to eq 204 }
+        it { expect(response.message).to eq 'No Content' }
+        it { expect(Rune.find_by id: existing_rune.id).to be_nil }
+      end
+
+      context 'when non admin' do
+        let!(:user) { create :user }
+
+        context 'when deleting another user rune' do
+          let!(:another_user_rune) { create :rune, user: create(:user) }
+          before do
+            delete api_rune_path(another_user_rune), nil, authorization: token_header(user)
+          end
+
+          it { expect(response).to be_forbidden }
+          it { expect(response).to match_response_schema 'forbidden' }
+        end
       end
     end
   end
