67a752eed7eb9c86675495f4ac596e6e71ee7c98
fusillicode <fusillicode@gmail.com>
2016-02-25 14:59:59 +0100

    Add initial seeds draft

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+# Seed users
+User.roles.keys.each do |role|
+  next if User.find_by username: role
+  User.create username: role, password: "#{role}pass", role: role
+end
+
+# Seed some runes with related random powers
+%w(ᚠ ᚢ ᚦ ᚨ ᚱ ᚲ ᚷ ᚹ ᚺ ᚾ ᛁ ᛃ ᛇ ᛈ ᛉ ᛊ ᛏ ᛒ ᛖ ᛗ ᛚ ᛜ ᛟ ᛞ).each do |rune_name|
+  Rune.find_or_create_by(name: rune_name,
+                         description: Faker::Hipster.sentences.first,
+                         # change to RAND() if you're on MySQL
+                         user: User.order('RANDOM()').first).tap do |rune|
+
+    rune.powers << rand(1..3).times.map do
+      Power.find_or_create_by name: Faker::Superhero.power,
+                              description: Faker::Hipster.sentences.first
+    end
+
+    rune.save
+  end
+end
