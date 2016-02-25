ecce9acebe9c8ccce18819b6e740f0f1a45aee0c
fusillicode <fusillicode@gmail.com>
2016-02-25 13:29:16 +0100

    Add specs for ownable powers and runes

--- spec/models/power_spec.rb
+++ spec/models/power_spec.rb
@@ -4,6 +4,9 @@ describe Power do
   describe 'Validations' do
     it { is_expected.to validate_presence_of(:name) }
     it { create :power; is_expected.to validate_uniqueness_of(:name).scoped_to(:rune_id) }
-    it { should belong_to(:rune) }
+    it { is_expected.to belong_to(:rune) }
   end
+
+  it { is_expected.to delegate_method(:owner).to(:rune) }
+  it_behaves_like 'ownable', :power
 end
--- spec/models/rune_spec.rb
+++ spec/models/rune_spec.rb
@@ -4,6 +4,8 @@ describe Rune do
   describe 'Validations' do
     it { is_expected.to validate_presence_of(:name) }
     it { create :rune; is_expected.to validate_uniqueness_of(:name) }
-    it { should have_many(:powers).dependent(:destroy) }
+    it { is_expected.to have_many(:powers).dependent(:destroy) }
   end
+
+  it_behaves_like 'ownable', :power
 end
