2256a8b06ae890cfed7a71aed76c7ced3ba071de
fusillicode <fusillicode@gmail.com>
2016-02-25 09:09:08 +0100

    Refactor routes with initial powers resource draft

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,12 @@ require 'api_constraints'
 Rails.application.routes.draw do
   namespace :api, defaults: { format: 'json' } do
     scope module: :v1, constrains: ApiConstraints.new(version: 1) do
-      resources :users, only: [:index, :show, :update, :destroy] do
+      resources :users, except: %i(new create edit) do
         get :token, on: :collection
       end
-      resources :runes
+      resources :runes, except: %i(new edit) do
+        resources :powers, except: %i(new edit), shallow: true
+      end
       match '*unmatched_route', controller: 'base', to: '#routing_error', via: :all
     end
   end
