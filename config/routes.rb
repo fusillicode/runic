02a3b5bdd596d0d85e1eb477faf059ebea36c194
fusillicode <fusillicode@gmail.com>
2016-02-26 09:19:44 +0100

    Fix routing with routes displaying

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   root to: 'welcome#index'
 
   namespace :api, defaults: { format: 'json' } do
+    get '/', to: '/welcome#index', via: :all
+
     scope module: :v1, constrains: ApiConstraints.new(version: 1) do
       resources :users, except: %i(new create edit) do
         get :token, on: :collection
@@ -11,7 +13,8 @@ Rails.application.routes.draw do
       resources :runes, except: %i(new edit) do
         resources :powers, except: %i(new edit), shallow: true
       end
-      match '*unmatched_route', controller: 'base', to: '#routing_error', via: :all
     end
+
+    get '*unmatched_route', to: 'base#routing_error', via: :all
   end
 end
